450182bf9c003eb0023a09136ace1f5349c6c763
MisaelSalazar <MisaelSalazarOf@protonmail.com>
2026-08-05 18:55:39 -0600

    Login Layout Finshed

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,6 +1,27 @@
+import Header from "../../components/Header/Header";
+
 function Login() {
-    return(
-        <h1>Este es el login</h1>
+    return (
+        <>
+            <Header title="Esteticas Caninas" />
+            <div className="row">
+
+                <div className="col-md-4"></div>
+
+                <form className="w-25 card m-4 p-3" action="">
+                    <center>
+                        <h2>Inicio de Sesión</h2>
+                    </center>
+                    <label htmlFor="">Usuario:</label>
+                    <input className="form-control" type="text" />
+
+                    <label className="mt-4" htmlFor="">Contraseña:</label>
+                    <input className="form-control" type="password" />
+
+                    <input className="btn btn-primary mt-4" type="button" value="Ingresar" />
+                </form>
+            </div>
+        </>
     );
 }
 
