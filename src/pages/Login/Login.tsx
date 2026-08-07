151280bf6fd3143d812512eb487b37d5057d38c9
MisaelSalazar <MisaelSalazarOf@protonmail.com>
2026-08-06 18:10:01 -0600

    Custom Hook Finished

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,6 +1,10 @@
 import Header from "../../components/Header/Header";
+import { LoginForm } from "./LoginForm";
+import { useState } from "react";
 
 function Login() {
+    const [userName, setUserName] = useState<string>("");
+    const [userPass, setUserPass] = useState<string>("");
     return (
         <>
             <Header title="Esteticas Caninas" />
@@ -13,12 +17,30 @@ function Login() {
                         <h2>Inicio de Sesión</h2>
                     </center>
                     <label htmlFor="">Usuario:</label>
-                    <input className="form-control" type="text" />
+                    <input
+                        className="form-control"
+                        type="text"
+                        onChange={(e) => { setUserName(e.target.value) }}
+                    />
 
                     <label className="mt-4" htmlFor="">Contraseña:</label>
-                    <input className="form-control" type="password" />
+                    <input
+                        className="form-control"
+                        type="password"
+                        onChange={(e) => { setUserPass(e.target.value) }}
+                    />
 
-                    <input className="btn btn-primary mt-4" type="button" value="Ingresar" />
+                    <input
+                        className="btn btn-primary mt-4"
+                        type="submit"
+                        value="Ingresar"
+                        onClick={() =>
+                            LoginForm({
+                                user: userName,
+                                password: userPass
+                            })
+                        }
+                    />
                 </form>
             </div>
         </>
